3bc5a454e690d2530c8620a2dbf625a5493b2da2
ajimae <meeky.ae@gmail.com>
2018-08-16 01:49:10 +0100

    reuploaded files to gh-pages

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ function toggleClass() {
 }
 
 document.querySelector('.menu-toggle').addEventListener('click', toggleClass);
+
+var show = document.querySelector('div#edit');
+document.querySelector('span#toggle').addEventListener('click', function() {
+    if(show.style.visibility == "") {
+        document.querySelector('#edit').style.visibility = "visible";
+    }else if(document.querySelector('#edit').style.visibility == "visible") {
+        document.querySelector('#edit').style.visibility = "";
+    }
+});
+
+document.querySelector('#close').addEventListener('click', function() {
+    document.querySelector('#edit').style.visibility = "";
+});
