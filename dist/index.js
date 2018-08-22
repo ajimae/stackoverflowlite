c9f126240a0e2e87d3a8e7fc1ff9bf1caf534c0b
ajimae <meeky.ae@gmail.com>
2018-08-22 19:18:17 +0100

    implemented the backend feedback from my LFA on GET Questions [finishes #159959736]

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+var myName = function myName(name) {
+	return 'Meeky';
+};
+
+exports.default = myName;
+//# sourceMappingURL=index.js.map
