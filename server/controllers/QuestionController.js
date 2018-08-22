c4ae61d7fc9e9549e1c069680d2ad9a0e561896f
ajimae <meeky.ae@gmail.com>
2018-08-22 18:30:24 +0100

    implemented the backend feedback from my LFA on POST Questions

--- server/controllers/QuestionController.js
+++ server/controllers/QuestionController.js
@@ -1,49 +1,9 @@
 import Questions from '../data/question.structures';
-import { isNumber } from 'util';
 
 class QuestionController {
-    getAllQuestions = (req, res) => {
-        if(Questions.length == 0 || Questions.length == undefined) {
-            return res.status(404).json({
-                message: "No questions to display"
-            });
-        }
-
-        return res.status(200).json(Questions); // Ok
-    }
-
-    getQuestion = (req, res) => {
-        const _id = parseInt(req.params.questionId)
-        if( _id <= 0) {
-            return res.status(404).json({   // Not found
-                message: "No question found matching the specified ID",
-            });
-        }
-
-        if(Questions.length == 0 || Questions.length == undefined) {
-            return res.status(404).json({
-                message: "No questions to display"
-            });
-        }
-
-        if(isNaN(_id)) {
-            return res.status(400).json({
-                message: "Invalid questionID"
-            });
-        }
-        for(let value of Questions) {
-            if(value.id == _id) {
-                return res.status(200).json(Questions[_id]);
-            }
-        }
-
-        return res.status(404).json({
-            message: "Question not found matching the specified ID"
-        });
-    }
 
     createQuestion = (req, res) => {
-        if(!req.body.title || !req.body.description || !req.body.asker || !req.body.votes || !req.body.views || !req.body.tags || !req.body.timestamp) {
+        if(!req.body.title || !req.body.description) {
             return res.status(400).json({   // Bad request
                 message: "All fields are mandatory."
             });
@@ -51,6 +11,7 @@ class QuestionController {
         
         let x = 0;
         const newQuestion = {
+            
             id: x = (Questions.length == 0) ? 1 : parseInt(Questions[Questions.length - 1].id + 1),
             title: req.body.title,
             description: req.body.description,
@@ -67,69 +28,6 @@ class QuestionController {
         });
     }
 
-    editQuestion = (req, res) => { //Validate this method
-        const _id = parseInt(req.params.questionId);
-        if(!req.body.title || !req.body.description) {
-            return res.status(400).json({   // Bad Request
-                message: "The title and/or description cannot be empty."
-            });
-        }
-
-        if(isNaN(_id) || _id <= 0) {
-            return res.status(400).json({   // Bad request
-                message: "The requested update was not successful, the specified question ID is not valid",
-            });
-        }else {
-        }
-
-        if(_id != '' && !isNaN(_id) && _id > 0) {
-             for(let value of Questions) {
-                if(_id == value.id) {
-                    const updatedQuestion = Questions[(_id) - 1] = req.body;
-                    updatedQuestion.id = _id;
-                    updatedQuestion.timestamp = `Updated ${req.body.timestamp}mins ago`;
-                    return res.status(200).json({
-                        message: "Question successfully updated."
-                    });
-                }
-            }
-
-            return res.status(400).json({   // Bad request found
-                message: "The requested update was not successful, the specified question ID was not found on this server",
-            });
-        }
-    }
-    
-
-    deleteQuestion = (req, res) => {
-        const index = parseInt(req.params.questionId);
-        if(index <= 0) {
-            return res.status(404).json({   // Not found
-                message: "The question could not be deleted, the specified question ID was not found on this server",
-            });
-        }
-        if(isNaN(index)) {
-            return res.status(404).json({   // Not found
-                message: "Invalid question ID",
-            });
-        }
-
-        for(let value of Questions) {
-            if(value.id == index) {
-                let needle = Questions.indexOf(value);
-                const result = Questions.splice(needle, 1);  // Write a code to delete the associated answers
-                if(!result) {
-                    return res.status(501).json({   // Not implemented
-                        message: "Question could not be deleted at this time, please try again later."
-                    });
-                }
-            }
-        }
-        
-        return res.status(200).json({
-            message: "The question was successfully deleted."
-        });
-    }
 }
 
 export default new QuestionController;
--- test/index.spec.js
+++ test/index.spec.js
@@ -9,137 +9,14 @@ import server from '../server/index';
 const should = chai.should();
 chai.use(chaiHttp);
 
-
-
-// User route controller
-describe('User Route Controller', () => {
-	it('Should create new user returning status code 201', (done) => {
-		const values = {
-			id: 1,
-			email: 'me@gmail.com',
-			username: 'meeky',
-			password: 'password'
-		};
-		chai.request(server)
-		.post('/api/v1/users/signup')
-		.send(values)
-		.end((err, res) => {
-			res.should.have.status(201);
-			res.body.should.be.a('object');
-			done();
-		});
-	});
-	it('Should return 400 for incomplete user signup info', (done) => {
-		const values = {
-			id: 1,
-			email: 'me@gmail.com',
-			username: '',
-			password: 'password'
-		};
-		chai.request(server)
-		.post('/api/v1/users/signup')
-		.send(values)
-		.end((err, res) => {
-			res.should.have.status(400);
-			res.should.be.json;
-			res.body.should.be.a('Object');
-			done();
-		});
-	});
-	it('Should return 400 for incomplete user login info', (done) => {
-		const values = {
-			email: 'me@gmail.com',
-			password: '',
-		};
-		chai.request(server)
-		.post('/api/v1/users/login')
-		.send(values)
-		.end((err, res) => {
-			res.should.have.status(400);
-			res.should.be.json;
-			res.body.should.be.a('Object');
-			done();
-		});
-	});
-	// it('Should return 404 for empty user list/table', (done) => {
-	// 	const values = {
-	// 		email: "johndoe@mail.com",
-	// 		password: 12345
-	// 	};
-	// 	chai.request(server)
-	// 	.get('/api/v1/users')
-	// 	.send(values)
-	// 	.end((err, res) => {
-	// 		res.should.have.status(404);
-	// 		res.body.should.be.a('Object');
-	// 		done();
-	// 	});
-	// });
-	// it('Login an existing user', (done) => {
-	// 	const values = {
-	// 		email: "johndoe@mail.com",
-	// 		password: 12345
-	// 	};
-	// 	chai.request(server)
-	// 	.post('/api/v1/users/login')
-	// 	.send(values)
-	// 	.end((err, res) => {
-	// 		console.log(res.body);
-	// 		res.should.have.status(200);
-	// 		res.body.should.be.a('Object');
-	// 		done();
-	// 	});
-	// });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Home route controller
-describe('Home route controller', () => {
-	it('Should return the home route at /', (done) => {
-		chai.request(server)
-		.get('/')
-		.end((req, res) => {
-			res.should.have.status(200);
-			res.should.be.json;
-			res.body.should.be.a('Object');
-			done();
-		});
-	});
-});
-
-
 // Questions controller
 describe('Question route controller', () => {
-	it('Should return all array of questions', (done) => {
-		chai.request(server)
-		.get('/api/v1/questions')
-		.end((req, res) => {
-			res.should.have.status(200);
-			res.should.be.json;
-			res.body.should.be.a('Array');
-			res.body[0].id.should.be.a('Number');
-			res.body[0].title.should.be.a('String');
-			done();
-		});
-	});
-	it('should return 200 for POST /question', (done) => {
+	it('should return 201 for POST /questions', (done) => {
 		const values = {
 			id: 1,
 			title: 'What is 4.5! (i.e. 4.5 factorial)',
 			description: 'How do I find the factorial of a decimal or fractional number',
-			// answers: 8,
+			answers: 8,
 			asker: 'Meeky',
 			votes: 0,
 			views: 12,
@@ -150,32 +27,27 @@ describe('Question route controller', () => {
 		.post('/api/v1/questions')
 		.send(values)
 		.end((err, res) => {
-			res.should.have.status(400);
+			res.should.have.status(201);
 			res.should.be.json;
 			res.body.should.be.a('Object');
 			res.body.message.should.be.a('string');
 			done();
 		});
 	});
-	// it('Should return 404 for a an empty or null questions array/array', (done) => {
-	// 	chai.request(server)
-	// 	.get('/api/v1/questions')
-	// 	.end((req, res) => {
-	// 		res.should.have.status(404);
-	// 		res.should.be.json;
-	// 		res.body.should.be.a('Object');
-	// 		res.body[0].id.should.be.a('Number');
-	// 		res.body[0].title.should.be.a('String');
-	// 		done();
-	// 	});
-	// });
-	it('Should return 400 for PUT /questions with incomplete parameters', (done) => {
+	it('Should return 400 for POST /questions with incomplete parameters', (done) => {
 		const values = {
+			id: 1,
 			title: 'What is 4.5! (i.e. 4.5 factorial)',
 			description: '',
+			answers: 8,
+			asker: 'Meeky',
+			votes: 0,
+			views: 12,
+			tags: 'javascript, node.js, json',
+			timestamp: 'asked 2hrs ago'
 		};
 		chai.request(server)
-		.put('/api/v1/questions/1')
+		.post('/api/v1/questions')
 		.send(values)
 		.end((err, res) => {
 			res.should.have.status(400);
@@ -183,100 +55,27 @@ describe('Question route controller', () => {
 			done();
 		});
 	});
-	it('Should return 200 for PUT /questions with title and description', (done) => {
+	it('should return 201 for POST /questions with no ID value', (done) => {
 		const values = {
 			id: 1,
 			title: 'What is 4.5! (i.e. 4.5 factorial)',
 			description: 'How do I find the factorial of a decimal or fractional number',
+			answers: 8,
+			asker: 'Meeky',
+			votes: 0,
+			views: 12,
+			tags: 'javascript, node.js, json',
+			timestamp: 'asked 2hrs ago',
 		};
 		chai.request(server)
-		.put('/api/v1/questions/1')
+		.post('/api/v1/questions')
 		.send(values)
 		.end((err, res) => {
-			res.should.have.status(200);
+			res.should.have.status(201);
 			res.should.be.json;
+			res.body.should.be.a('Object');
+			res.body.message.should.be.a('string');
 			done();
 		});
 	});
-    it('should return 200 for PUT /questions/:quesionId with a valid ID', (done) => {
-		const values = {
-			title: 'What is 4.5! (i.e. 4.5 factorial)',
-			description: 'How do I find the factorial of a decimal or fractional number',
-		};
-		chai.request(server)
-		.put('/api/v1/questions/1')
-		.send(values)
-		.end((err, res) => {
-			res.should.have.status(200);
-			res.body.should.be.a('object');
-			res.should.be.json;
-			done();
-		});
-    });
-	it('should return 400 for PUT /question/:questionId with an invalid quesion ID', (done) => {
-		const values = {
-			title: 'How do I master Javascript?',
-			description: 'Use MDN web portal for your reference and tutorials',
-		};
-		chai.request(server)
-		.put('/api/v1/questions/a')
-		.send(values)
-		.end((err, res) => {
-			res.should.have.status(400);
-			res.body.should.be.a('object');
-			res.should.be.json;
-			done();
-		});
-    });
-    it('should return 404 for PUT /question/questionId for ID that is out of range', (done) => {
-        chai.request(server)
-        .put('/api/v1/questions/-5')
-        .end((err, res) => {
-            res.should.have.status(400);
-            res.body.should.be.a('object');
-            res.should.be.json;
-            done();
-        });
-    });
-	it('should return 404 for GET /questions/:questionId with an invalid ID', (done) => {
-		chai.request(server)
-		.get('/api/v1/questions/a')
-		.end((err, res) => {
-			res.should.have.status(400);
-			res.body.should.be.a('object');
-			res.should.be.json;
-			done();
-		});
-	});
-	it('should return 404 for GET /questions/:questionId with an unknown ID', (done) => {
-		chai.request(server)
-		.get('/api/v1/questions/100')
-		.end((err, res) => {
-			res.should.have.status(404);
-			res.body.should.be.a('object');
-			res.should.be.json;
-			done();
-		});
-	});
-	// it('should return 404 for GET /questions when there is empty list or table', (done) => {
-	// 	chai.request(server)
-	// 	.get('/api/v1/questions')
-	// 	.end((err, res) => {
-	// 		res.should.have.status(404);
-	// 		res.body.should.be.a('object');
-	// 		res.should.be.json;
-	// 		done();
-	// 	});
-	// });
-	// it('should return 404 for GET /questions/:questionId when there is empty list or table', (done) => {
-	// 	chai.request(server)
-	// 	.get('/api/v1/questions/1')
-	// 	.end((err, res) => {
-	// 		res.should.have.status(404);
-	// 		res.body.should.be.a('object');
-	// 		res.should.be.json;
-	// 		done();
-	// 	});
-	// });
-	
 });
