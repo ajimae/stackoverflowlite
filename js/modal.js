a4e6fa83a4e98b6cd7388d0699155879a430d147
ajimae <meeky.ae@gmail.com>
2018-08-22 15:06:26 +0100

    final corrections and implementation of LFA feedback

--- js/modal.js
+++ js/modal.js
@@ -11,6 +11,25 @@ document.addEventListener('click', function (e) {
         }
     }
 
+    
+    if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'modal1') {
+        if (target.hasAttribute('data-target')) {
+            var m_ID = target.getAttribute('data-target');
+            document.getElementById('put').innerHTML = document.querySelector('.editProfile').innerHTML;
+            document.getElementById(m_ID).classList.add('open');
+            e.preventDefault();
+        }
+    }
+
+    if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'modal2') {
+        if (target.hasAttribute('data-target')) {
+            var m_ID = target.getAttribute('data-target');
+            document.getElementById('put').innerHTML = document.querySelector('.postQuestions').innerHTML;
+            document.getElementById(m_ID).classList.add('open');
+            e.preventDefault();
+        }
+    }
+
     // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
     if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'modal') || target.classList.contains('modal')) {
         var modal = document.querySelector('[class="modal open"]');
