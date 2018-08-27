6bb2317082650c74a24bfd9e7d0681fb50c0dceb
ajimae <meeky.ae@gmail.com>
2018-08-27 09:26:41 +0100

    deploying to heroku

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import session from 'express-session';
+//import session from 'express-session';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import routes from './routes/routes';
