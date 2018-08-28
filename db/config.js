761dde56e658cc0d63379f20b8275a03a11c0a10
ajimae <meeky.ae@gmail.com>
2018-08-28 03:09:59 +0100

    deploying to heroku

--- db/config.js
+++ db/config.js
@@ -4,22 +4,20 @@ dotEnv.config();
 
 const databaseConfig = {};
 
-
-
-databaseConfig.development = {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: process.env.SSL,
-};
-
-
-// databaseConfig.test = {
-//     //connectionString: process.env.STACK_LITE,
-//     databaseConfig: "postgres://mlnbloqjrdgbiz:0f2522ad45392f1afb072f2d1edab52d07fb6bdca24602bdde3b0ff64f0f3ad7@ec2-54-243-216-33.compute-1.amazonaws.com:5432/dbt5gu8kib8g1b"
+// databaseConfig.development = {
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+//   ssl: process.env.SSL,
 // };
+
+
+databaseConfig.test = {
+    //connectionString: process.env.STACK_LITE,
+    databaseConfig: "postgres://mlnbloqjrdgbiz:0f2522ad45392f1afb072f2d1edab52d07fb6bdca24602bdde3b0ff64f0f3ad7@ec2-54-243-216-33.compute-1.amazonaws.com:5432/dbt5gu8kib8g1b"
+};
 
 
 export default databaseConfig;
