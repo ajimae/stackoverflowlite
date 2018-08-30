a0822770bf30ed2c260902bcab739a6170d60b2a
ajimae <meeky.ae@gmail.com>
2018-08-30 18:26:15 +0100

    Deploying to heroku

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,9 @@
 import auth from '../middlewares/auth';
-//import validate from '../middlewares/validation';
-import validateUser from '../middlewares/userValidation';
-import UserController from '../controllers/UserController';
+import validate from '../middlewares/Validation';
+import validateUser from '../middlewares/UserValidation';
+import userController from '../controllers/UserController';
 import HomeController from '../controllers/HomeController';
-import validateUser2 from '../middlewares/loginValidation';
+import validateUser2 from '../middlewares/LoginValidation';
 import AnswerController from '../controllers/AnswerController';
 import QuestionController from '../controllers/QuestionController';
 
@@ -13,8 +13,8 @@ export default (server) => {
     server.get('/api', new HomeController().home);
     server.get('/api/v1', new HomeController().home);
     server.get('/api/auth', new HomeController().home);
-    server.post('/api/v1/auth/signup', validateUser, new UserController().createUser);
-    server.post('/api/v1/auth/login', validateUser2, new UserController().loginUser);
+    server.post('/api/v1/auth/login', validateUser2, new userController().loginUser);
+    server.post('/api/v1/auth/signup', validateUser, new userController().createUser);
     server.post('/api/v1/questions', auth.verifyUserToken, validate.validateQuestions, new QuestionController().createQuestion);
     server.get('/api/v1/questions', new QuestionController().getAllQuestions);
     server.get('/api/v1/questions/:questionId', new QuestionController().getQuestion);
